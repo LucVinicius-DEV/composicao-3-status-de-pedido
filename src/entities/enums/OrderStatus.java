75e85c3f927417710010b42a57ae6872d0cde6f4
Lucas <us3.lucas@gmail.com>
2025-05-20 16:52:50 -0300

    Update OrderStatus.java

--- src/entities/enums/OrderStatus.java
+++ src/entities/enums/OrderStatus.java
@@ -1,10 +1,10 @@
 package entities.enums;
 
+// Enum que define os possíveis estados de um pedido
 public enum OrderStatus {
-
-  PENDING_PAYMENT,
-  PROCESSING,
-  SHIPPED,
-  DELIVERED;
   
+  PENDING_PAYMENT, // Pagamento pendente
+  PROCESSING,      // Em processamento
+  SHIPPED,         // Enviado
+  DELIVERED;       // Entregue
 }
