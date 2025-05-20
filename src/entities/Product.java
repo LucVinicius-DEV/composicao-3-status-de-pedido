b659c3b8b7d9cc459128854768797cc8b2882c15
Lucas <us3.lucas@gmail.com>
2025-05-20 17:04:42 -0300

    Update Product.java

--- src/entities/Product.java
+++ src/entities/Product.java
@@ -1,28 +1,36 @@
 package entities;
 
+// Classe que representa um produto no sistema
 public class Product {
-
-        private String name;
-        private Double price;
-
-        public Product(String name, Double price) {
-                this.name = name;
-                this.price = price;
-        }
-
-        public String getName() {
-                return name;
-        }
-
-        public void setName(String name) {
-                this.name = name;
-        }
-
-        public Double getPrice() {
-                return price;
-        }
-
-        public void setPrice(Double price) {
-                this.price = price;
-        }
+  
+  // Atributos básicos do produto
+  private String name;      // Nome do produto
+  private Double price;     // Preço unitário
+  
+  // Construtor padrão
+  public Product() {
+  }
+  
+  // Construtor com parâmetros
+  public Product(String name, Double price) {
+    this.name = name;
+    this.price = price;
+  }
+  
+  // Métodos getters e setters
+  public String getName() {
+    return name;
+  }
+  
+  public void setName(String name) {
+    this.name = name;
+  }
+  
+  public Double getPrice() {
+    return price;
+  }
+  
+  public void setPrice(Double price) {
+    this.price = price;
+  }
 }
