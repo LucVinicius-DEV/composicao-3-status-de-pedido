4b05dfcf5c01e44b34ee7de2a6e9613b096886c0
Lucas <us3.lucas@gmail.com>
2025-05-20 17:06:31 -0300

    Update OrderItem.java

--- src/entities/OrderItem.java
+++ src/entities/OrderItem.java
@@ -1,54 +1,53 @@
 package entities;
 
+// Classe que representa um item de pedido (linha de pedido)
 public class OrderItem {
-
-        private Integer quantity;
-        private Double price;
-
-        private Product product;
-
-        public OrderItem(Integer quantity, Double price, Product product) {
-                this.quantity = quantity;
-                this.price = price;
-                this.product = product;
-        }
-
-        public Integer getQuantity() {
-                return quantity;
-        }
-
-        public void setQuantity(Integer quantity) {
-                this.quantity = quantity;
-        }
-
-        public Double getPrice() {
-                return price;
-        }
-
-        public void setPrice(Double price) {
-                this.price = price;
-        }
-
-        public Product getProduct() {
-                return product;
-        }
-
-        public void setProduct(Product product) {
-                this.product = product;
-        }
-
-        public double subTotal() {
-                return price * quantity;
-        }
-
-        @Override
-        public String toString() {
-                return product.getName() 
-                                + ", $" 
-                                + String.format("%.2f", price) 
-                                + ", Quantity: " 
-                                + quantity + 
-                                ", Subtotal: $" 
-                                + String.format("%.2f", subTotal());
-        }
+  
+  // Atributos
+  private Integer quantity;   // Quantidade do produto
+  private Double price;       // Preço unitário (cópia do preço do produto no momento da compra)
+  
+  // Relação de composição com Product
+  private Product product;    // Produto associado ao item
+  
+  // Construtor padrão
+  public OrderItem() {
+  }
+  
+  // Construtor com parâmetros
+  public OrderItem(Integer quantity, Double price, Product product) {
+    this.quantity = quantity;
+    this.price = price;
+    this.product = product;
+  }
+  
+  // Métodos getters e setters
+  public Integer getQuantity() {
+    return quantity;
+  }
+  
+  public void setQuantity(Integer quantity) {
+    this.quantity = quantity;
+  }
+  
+  public Double getPrice() {
+    return price;
+  }
+  
+  public void setPrice(Double price) {
+    this.price = price;
+  }
+  
+  public Product getProduct() {
+    return product;
+  }
+  
+  public void setProduct(Product product) {
+    this.product = product;
+  }
+  
+  // Método para calcular o subtotal do item (preço * quantidade)
+  public Double subTotal() {
+    return price * quantity;
+  }
 }
