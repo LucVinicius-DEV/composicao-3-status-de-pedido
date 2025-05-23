599a106123d02d2a24fe7d9c02361b5db001f42d
Lucas <us3.lucas@gmail.com>
2025-05-23 10:44:36 -0300

    Update OrderItem.java

--- src/entities/OrderItem.java
+++ src/entities/OrderItem.java
@@ -50,4 +50,16 @@ public class OrderItem {
   public Double subTotal() {
     return price * quantity;
   }
-}
+
+
+@Override
+  public String toString() {
+    return getProduct().getName()
+      + ", $"
+      + String.format("%.2f", price)
+      + ", Quantity: "
+      + quantity
+      + ", Subtotal: $"
+      + String.format("%.2f", subTotal());
+  }
+}  
